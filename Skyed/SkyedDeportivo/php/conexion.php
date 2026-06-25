f8f294f79003f5bbd593f5ffaee8b3e3704e5ce8
yisethsara094-boop <yisethsara094@gmail.com>
2026-06-25 13:39:34 -0500

    conexion

--- Skyed/SkyedDeportivo/php/conexion.php
+++ Skyed/SkyedDeportivo/php/conexion.php
@@ -1,12 +1,12 @@
 <?php
-$DB_HOST = 'localhost';
+$DB_HOST = '127.0.0.1';
 $DB_NAME = 'skyed';
 $DB_USER = 'root';
-$DB_PASS = '2907sindy;
+$DB_PASS = '123456';
 
 try {
     $pdo = new PDO(
-        "mysql:host=$DB_HOST;port=3307;dbname=$DB_NAME;charset=utf8mb4",
+        "mysql:host=$DB_HOST;port=3306;dbname=$DB_NAME;charset=utf8mb4",
         $DB_USER, $DB_PASS,
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
